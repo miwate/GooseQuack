c194ebef11cf94edbf438cc6e30e9e05c9725b3c
William (miwate) <williamduo53@gmail.com>
2025-12-01 13:24:07 +0100

    started VersSacADos

---
+++ src/main/java/fr/GooseQuack/VersSacADos.java
@@ -0,0 +1,23 @@
+package fr.GooseQuack;
+
+import java.util.List;
+
+import fr.GooseQuack.equipe.Projet;
+import fr.GooseQuack.sacados.SacADos;
+
+/**
+ * Classe pour convertir des projets en sac à dos
+ * 
+ * @author Drys (lidr05)
+ * @author William (miwate)
+ * 
+ * @version 1.0
+ */
+public class VersSacADos {
+    
+    public static SacADos versTypeCout(List<Projet> projets, int budgetEconomique, int budgetSocial, int budgetEnvironnemental) {
+        int budgets[] = new int[] {budgetEconomique, budgetSocial, budgetEnvironnemental};
+        // et là faut continuer
+    }
+
+}
