e8148fb3827737b205403bcf0f622244f44a0334
William (miwate) <williamduo53@gmail.com>
2025-12-04 12:10:43 +0100

    updated VersSacADos (implemented constructor)

--- src/main/java/fr/GooseQuack/VersSacADos.java
+++ src/main/java/fr/GooseQuack/VersSacADos.java
@@ -1,8 +1,10 @@
 package fr.GooseQuack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.GooseQuack.equipe.Projet;
+import fr.GooseQuack.sacados.Objet;
 import fr.GooseQuack.sacados.SacADos;
 
 /**
@@ -16,8 +18,35 @@ import fr.GooseQuack.sacados.SacADos;
 public class VersSacADos {
     
     public static SacADos versTypeCout(List<Projet> projets, int budgetEconomique, int budgetSocial, int budgetEnvironnemental) {
+        
+        if (projets.isEmpty() || projets == null) {
+            throw new IllegalArgumentException("La liste des projets doit être non null et non vide");
+        }
+
+        if (budgetEconomique < 0 || budgetSocial < 0 || budgetEnvironnemental < 0) {
+            throw new IllegalArgumentException("Les budgets doivent être positifs");
+        }
+
         int budgets[] = new int[] {budgetEconomique, budgetSocial, budgetEnvironnemental};
-        // et là faut continuer
+        int dimension = budgets.length;
+
+        List<Objet> objets = new ArrayList<>();
+
+        for (Projet p : projets) {
+            if (p == null) {
+                throw new IllegalArgumentException("Chaque projet de la liste doit être non null");
+            }
+
+            int utilite = p.getBenefice();
+
+            int couts[] = p.getCouts();
+
+            objets.add(new Objet(utilite, couts));
+        }
+
+        return new SacADos(dimension, budgets, objets);
+
+
     }
 
 }
