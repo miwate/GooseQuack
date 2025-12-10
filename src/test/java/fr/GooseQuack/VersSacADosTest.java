918c77eabb6241bdcce3cad0506f449b03209e4f
William (miwate) <williamduo53@gmail.com>
2025-12-10 15:57:19 +0100

    added junit tests for versSecteur + added one junit test for versTypeCout

--- src/test/java/fr/GooseQuack/VersSacADosTest.java
+++ src/test/java/fr/GooseQuack/VersSacADosTest.java
@@ -72,7 +72,35 @@ class VersSacADosTest {
         projetsTest.add(pAttractivite);
     }
 
-    // ------------- versTypeCout -------------
+    // ------------------------ versTypeCout --------------------------
+
+    /**
+     * versTypeCout check de la création d'un projet seul
+     */
+    @Test
+    @DisplayName("versTypeCout : check de la création d'un projet seul")
+    void testVersTypeCoutCheckSolo() {
+        List<Projet> solo = new ArrayList<>();
+        Projet p = new Projet("solo", "solo", Secteur.ATTRACTIVITE_ECONOMIQUE);
+
+        p.setBenefice(84952130);
+        p.setCout(Cout.ECONOMIQUE, 1);
+        p.setCout(Cout.SOCIAL, 1);
+        p.setCout(Cout.ENVIRONNEMENTAL, 1);
+        solo.add(p);
+
+        SacADos sac = VersSacADos.versTypeCout(solo, 1, 1, 1);
+        Objet obj = sac.getObjets().get(0);
+
+        assertNotNull(sac);
+
+        assertEquals(3, sac.getDimension());
+        assertEquals(1, sac.getObjets().size());
+
+        assertEquals(84952130, obj.getUtilite());
+
+        assertArrayEquals(new int[]{1, 1, 1}, obj.getCouts());
+    }
 
     /**
      * Test de versTypeCout check de la création
@@ -108,6 +136,8 @@ class VersSacADosTest {
         assertEquals(7500, obj5.getUtilite());
         assertArrayEquals(new int[]{2200, 1000, 1400}, obj5.getCouts());
     }
+
+    
 
     /**
      * Test de versTypeCout pour liste null
@@ -277,6 +307,216 @@ class VersSacADosTest {
         Objet obj = sac.getObjets().get(0);
         assertEquals(1000001, obj.getUtilite());
         assertArrayEquals(new int[]{0, 0, 0}, obj.getCouts());
+    }
+
+
+
+    // ----------------------- versSecteur ------------------------
+
+
+    /**
+     * versSecteur : check un projet uniquement
+     */
+    @Test
+    @DisplayName("versSecteur : ucheck un projet uniquement")
+    void testVersSecteurCheckSolo() {
+        List<Projet> solo = new ArrayList<>();
+        Projet p = new Projet("solo", "solo", Secteur.ATTRACTIVITE_ECONOMIQUE);
+
+        p.setBenefice(84952130);
+        p.setCout(Cout.ECONOMIQUE, 1);
+        p.setCout(Cout.SOCIAL, 1);
+        p.setCout(Cout.ENVIRONNEMENTAL, 1);
+        solo.add(p);
+
+        SacADos sac = VersSacADos.versSecteur(solo, 1, 1, 1, 1, 1);
+        Objet obj = sac.getObjets().get(0);
+
+        assertNotNull(sac);
+
+        assertEquals(5, sac.getDimension());
+        assertEquals(1, sac.getObjets().size());
+
+        assertEquals(84952130, obj.getUtilite());
+        
+        assertArrayEquals(new int[]{0, 0, 0, 0, 1}, obj.getCouts());
+    }
+
+
+    /**
+     * versSecteur : check de la création du sac
+     */
+    @Test
+    @DisplayName("versSecteur : check de la création du sac")
+    void testVersSecteurCheck() {
+        SacADos sac = VersSacADos.versSecteur(projetsTest, 1, 1, 1, 1, 1);
+
+        assertNotNull(sac);
+        assertEquals(sac.getDimension(), 5);
+        assertArrayEquals(new int[]{1,1,1,1,1}, sac.getBudgets());
+
+        // Sport
+        Objet obj0 = sac.getObjets().get(0);
+        assertEquals(3000, obj0.getUtilite());
+        assertArrayEquals(new int[]{2000, 0, 0, 0, 0}, obj0.getCouts());
+
+        // Santé
+        Objet obj1 = sac.getObjets().get(2);
+        assertEquals(7000, obj1.getUtilite());
+        assertArrayEquals(new int[]{0, 2500, 0, 0, 0}, obj1.getCouts());
+
+        // Culture
+        Objet obj2 = sac.getObjets().get(1);
+        assertEquals(4500, obj2.getUtilite());
+        assertArrayEquals(new int[]{0, 0, 1000, 0, 0}, obj2.getCouts());
+
+
+        // Éducation
+        Objet obj3 = sac.getObjets().get(3);
+        assertEquals(6500, obj3.getUtilite());
+        assertArrayEquals(new int[]{0, 0, 0, 1800, 0}, obj3.getCouts());
+
+        // Actractivtité Économique
+        Objet obj4 = sac.getObjets().get(4);
+        assertEquals(7500, obj4.getUtilite());
+        assertArrayEquals(new int[]{0, 0, 0, 0, 2200}, obj4.getCouts());
+    }
+
+
+
+
+
+    /**
+     * versSecteur : liste null de projets
+     */
+    @Test
+    @DisplayName("versSecteur : liste null de projets")
+    void testVersSecteurNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            VersSacADos.versSecteur(null, 1, 1, 1, 1, 1);
+        });
+
+        assertEquals("La liste des projets doit être non null et non vide", exception.getMessage());
+
+    }
+
+    /**
+     * versSecteur : liste non null de projets mais un projet null
+     */
+    @Test
+    @DisplayName("versSecteur : liste non null de projets mais un projet null")
+    void testVersSecteurUnProjetNull() {
+        projetsTest.add(null);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            VersSacADos.versSecteur(projetsTest, 1, 1, 67, 1, 1);
+        });
+
+        assertEquals("Chaque projet de la liste doit être non null", exception.getMessage());
+    }
+    
+
+    // Je doute de la pertinence de tous les tester car c'est sur la même ligne de code mais c'est pas grave je le fais quand même
+    /**
+     * versSecteur : budget négatif pour sport
+     */
+    @Test
+    @DisplayName("versSecteur : budget négatif pour sport")
+    void testVersSecteurBudgetSportNeg() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            VersSacADos.versSecteur(projetsTest, -1, 0, 0, 0, 0);
+        });
+
+        assertEquals("Tous les budgets doivent être positifs", exception.getMessage());
+    }
+
+    /**
+     * versSecteur : budget négatif pour santé
+     */
+    @Test
+    @DisplayName("versSecteur : budget négatif pour santé")
+    void testVersSecteurBudgetSanteNeg() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            VersSacADos.versSecteur(projetsTest, 0, -1, 0, 0, 0);
+        });
+
+        assertEquals("Tous les budgets doivent être positifs", exception.getMessage());
+    }
+
+    /**
+     * versSecteur : budget négatif pour culture
+     */
+    @Test
+    @DisplayName("versSecteur : budget négatif pour culture")
+    void testVersSecteurBudgetCultNeg() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            VersSacADos.versSecteur(projetsTest, 0, 0, -1, 0, 0);
+        });
+
+        assertEquals("Tous les budgets doivent être positifs", exception.getMessage());
+    }
+
+    /**
+     * versSecteur : budget négatif pour éducation
+     */
+    @Test
+    @DisplayName("versSecteur : budget négatif pour éducation")
+    void testVersSecteurBudgetEduNeg() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            VersSacADos.versSecteur(projetsTest, 0, 0, 0, -1, 0);
+        });
+
+        assertEquals("Tous les budgets doivent être positifs", exception.getMessage());
+    }
+
+    /**
+     * versSecteur : budget négatif pour attractivité économique
+     */
+    @Test
+    @DisplayName("versSecteur : budget négatif pour attractivité économique")
+    void testVersSecteurBudgetAttNeg() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            VersSacADos.versSecteur(projetsTest, 0, 0, 0, 0, -1);
+        });
+
+        assertEquals("Tous les budgets doivent être positifs", exception.getMessage());
+    }
+
+
+
+    /**
+     * versSecteur : plusieurs projets mais du même secteur
+     */
+    @Test
+    @DisplayName("versSecteur : plusieurs projets du même secteur")
+    void testVersSecteurMemeSecteur() {
+        List<Projet> notreProjet = new ArrayList<>();
+
+        Projet p1 = new Projet("LKJFSDLHQF", "aaaaaaaaaaaaaaa", Secteur.EDUCATION);
+        p1.setBenefice(999);
+        p1.setCout(Cout.ECONOMIQUE, 999);
+        p1.setCout(Cout.SOCIAL, 999);
+        p1.setCout(Cout.ENVIRONNEMENTAL, 999);
+        notreProjet.add(p1);
+
+        Projet p2 = new Projet("LKJFSDLHQF", "bbbbbbbbbbbbbb", Secteur.EDUCATION);
+        p2.setBenefice(999);
+        p2.setCout(Cout.ECONOMIQUE, 999);
+        p2.setCout(Cout.SOCIAL, 999);
+        p2.setCout(Cout.ENVIRONNEMENTAL, 999);
+        notreProjet.add(p2);
+
+        SacADos sac = VersSacADos.versSecteur(notreProjet, 999, 999, 999, 999, 999);
+
+        Objet obj1 = sac.getObjets().get(0);
+
+        assertEquals(999, obj1.getUtilite());
+        assertArrayEquals(new int[]{0, 0, 0, 999, 0}, obj1.getCouts());
+
+        Objet obj2 = sac.getObjets().get(1);
+        
+        assertEquals(999, obj2.getUtilite());
+        assertArrayEquals(new int[]{0, 0, 0, 999, 0}, obj2.getCouts());
     }
 
 
