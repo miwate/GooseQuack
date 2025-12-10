5d278d9fca1dfc3cfafda8fcdc4c6e08ce430648
christianlikq-del <christianlikq@gmail.com>
2025-12-10 17:06:06 +0100

    Implement equals and hashCode methods in Objet class

--- src/main/java/fr/GooseQuack/sacados/Objet.java
+++ src/main/java/fr/GooseQuack/sacados/Objet.java
@@ -83,4 +83,21 @@ public class Objet {
         return couts.length;
     }
     
+    @Override
+    public bolean equals(Object o){
+    if (this==o){
+        return true;
+        }
+
+    if (o==null || getClass() != o.getClass()){
+        return false;
+        }
+    Objet objet=(Objet) o;
+    return utilite==objet.utilite && java.util.Arrays.equals(couts,objet.couts);
+    }
+    
+    @Override
+    public int hashCode(){
+    return java.util.objets.hash(utilite,java.util.Arrays.hashCode(couts));
+    }
 }
